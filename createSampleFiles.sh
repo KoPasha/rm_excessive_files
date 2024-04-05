e1fafa1bdcc2494154aabcd8b763031786ebd804
paul <kpp123@gmail.com>
2024-04-05 11:05:35 +0300

    shell script for creating test files

---
+++ createSampleFiles.sh
@@ -0,0 +1,14 @@
+touch fileStorage4_1c_all20240316030000.rar
+touch fileStorage4_1c_all20240323030000.rar
+touch fileStorage4_1c_all20240330030000.rar
+touch fileStorage4_1c_dif20240311230001.rar
+touch fileStorage4_1c_dif20240312230001.rar
+touch fileStorage4_1c_dif20240313230000.rar
+touch fileStorage4_1c_dif20240314230000.rar
+touch fileStorage4_1c_dif20240318230000.rar
+touch fileStorage4_1c_dif20240319230001.rar
+touch fileStorage4_1c_dif20240320230001.rar
+touch fileStorage4_1c_dif20240327230000.rar
+touch fileStorage4_1c_dif20240401230001.rar
+touch fileStorage4_1c_dif20240402230001.rar
+touch fileStorage4_1c_dif20240403230001.rar
